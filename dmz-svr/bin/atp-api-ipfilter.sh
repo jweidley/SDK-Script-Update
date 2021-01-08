e058f68f201213949ee170f1a44ae7f887331c68
jweidley <john.weidley@gmail.com>
2021-01-08 19:40:50 +0000

    21.1.3: api error checking

--- dmz-svr/bin/atp-api-ipfilter.sh
+++ dmz-svr/bin/atp-api-ipfilter.sh
@@ -2,10 +2,11 @@
 # Purpose: Process a sample web server log to gather source addresses and update ATP Cloud
 #	   IPFilter list.
 # Author: John Weidley (Sample code provided by Craig Dods, Michael Bergt)
-# Version: 0.1
+# Version: 0.2
 #################################################################################################
 # ChangeLog
 # 0.1: 5Jan21: Initial Release
+# 0.2: 8Jan21: added better error checking on API calls
 #################################################################################################
 
 ##########################
@@ -24,6 +25,59 @@ webSvrLog=/usr/local/bin/webserver.log
 # Local file that contains
 blockList=/tmp/blockList.txt
 
+################################################################
+# curlWithStatus: Run specific curl command with the options to
+#	see the status code and message.
+################################################################
+curlWithStatus() {
+	code=0
+	statuscode=0
+	body=0
+
+	# Determine whether to run the delete or add
+	if [ $1 == "delete" ]; then
+		statuscode=$(curl -s -k -w "%{http_code}" -XDELETE -H "Authorization: Bearer $APPToken" -F server='*' \
+			-o >(cat >/tmp/curl_body) \
+			"https://threat-api.sky.junipersecurity.net/v2/cloudfeeds/ipfilter/param/${BlockFeed}"
+			) || code="$?"
+	elif [ $1 == "add" ]; then
+		statuscode=$(curl -s -k -w "%{http_code}" -XPOST -H "Authorization: Bearer $APPToken" -F file=@${blockList} \
+			-o >(cat >/tmp/curl_body) \
+			"https://threat-api.sky.junipersecurity.net/v2/cloudfeeds/ipfilter/file/${BlockFeed}"
+			) || code="$?"
+	else
+		echo "ERROR: Unknown error...exiting!"
+	fi
+
+	body="$(cat /tmp/curl_body)"
+	#echo "statuscode : $statuscode"
+	#echo "exitcode : $code"
+	#echo "body : $body"
+
+	#####################################
+	# Check/Report status
+	# ! May not be 100% complete
+	#####################################
+	if [ $statuscode == "404" ]; then
+		echo "   + Feed not present (HTTP: $statuscode)...continuing"
+	elif [ $statuscode == "200" ] || [ $statuscode == "202" ]; then
+		echo "   + Feed operation successful. (HTTP: $statuscode)...continuing"
+		echo "   + Message: $body"
+	else
+		echo "   ---------------------------------------------------"
+		echo "   ERROR: Feed Update failed with error:"
+		echo "     - Status Code: $statuscode"
+		echo "     - $body"
+		echo "   ---------------------------------------------------"
+		echo " "
+		echo "    Troubleshooting:"
+		echo "    1. Ensure the APPToken variable is formatted correctly in the script"
+		echo "    2. Ensure the Application Token is still 'active' in ATP Cloud"
+		echo " "
+		echo "...Exiting..."
+		exit
+	fi
+}
 
 ##########################
 # Main
@@ -38,7 +92,7 @@ if [ $APPToken == "ADD-YOUR-TOKEN-HERE" ]; then
 	echo "ERROR: The APPToken variable hasn't been set!"
 	echo "  - Go to ATP Cloud WebUI (Administration > Application Tokens) to generate one"
 	echo "  - Add Token to /usr/local/bin/atp-api-ipfilter.sh script"
-    sleep 10
+	sleep 10
 	exit
 fi
 
@@ -54,14 +108,15 @@ fi
 sleep 5
 
 echo "- Deleting feed contents from ATP Cloud..."
-curl -k -v -XDELETE -H "Authorization: Bearer $APPToken" -F server='*' https://threat-api.sky.junipersecurity.net/v2/cloudfeeds/ipfilter/param/${BlockFeed}
+curlWithStatus delete
 
 sleep 10
 
 echo "- Updating feed contents in ATP Cloud..."
-curl -k -v -XPOST -H "Authorization: Bearer $APPToken" -F file=@${blockList} https://threat-api.sky.junipersecurity.net/v2/cloudfeeds/ipfilter/file/${BlockFeed}
+curlWithStatus add
 
 sleep 2
+
 # Clean up temporary file
 rm $blockList
 
